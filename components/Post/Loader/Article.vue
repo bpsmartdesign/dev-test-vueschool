f42ba62549651e9a02c49d087f6fafd313c1e79b
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2024-04-10 13:34:59 +0100

    feat(ux): create article skeleton loader

---
+++ components/Post/Loader/Article.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="max-w-screen-md mx-auto py-12 animate-pulse">
+    <h2 class="w-1/3 h-12 mb-2 bg-gray-200"></h2>
+    <p class="w-full h-8 bg-gray-200 p-2 mb-2"></p>
+    <div class="bg-gray-200 w-full h-[400px]"></div>
+    <div
+      class="w-full flex items-center justify-between gap-4 border-b border-gray-200 py-4"
+    >
+      <div class="w-1/6 flex items-center gap-2 text-xs text-gray-400 pt-2">
+        <div class="h-4 w-4 bg-gray-200 rounded-sm"></div>
+        <p class="w-full h-2 bg-gray-200"></p>
+      </div>
+      <div class="w-1/6 flex items-center gap-2 text-xs text-gray-400 pt-2">
+        <div class="h-4 w-4 bg-gray-200 rounded-sm"></div>
+        <p class="w-full h-2 bg-gray-200"></p>
+      </div>
+    </div>
+    <p class="w-full h-8 bg-gray-200 p-2 mb-2"></p>
+    <p class="w-3/4 h-4 bg-gray-200 p-2 mb-2"></p>
+    <p class="w-1/3 h-2 bg-gray-200 p-2 mb-2"></p>
+  </div>
+</template>
