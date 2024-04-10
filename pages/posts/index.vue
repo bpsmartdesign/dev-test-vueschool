216c2ee8737a7986625bdf563ad3d48145fab38a
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2024-04-10 06:04:57 +0100

    feat: sort posts

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,10 +2,23 @@
 import type { Pagination } from "~/types";
 
 const pagination = ref<Pagination>({ currentPage: 1, perPage: 12, total: 0 });
+const newestFirst = ref<boolean>(true);
+const blogView = ref<"grid" | "list">("grid");
 
 const offset = computed(
   () => (pagination.value.currentPage - 1) * pagination.value.perPage
 );
+const order = computed(() =>
+  newestFirst.value ? "newestFirst" : "oldestFirst"
+);
+const query = computed(() => ({
+  limit: pagination.value.perPage,
+  offset,
+  order,
+  include: "user",
+  select:
+    "id,title,excerpt,publishedAt,image,user.firstName,user.lastName,user.avatar,user.email",
+}));
 
 const { refresh: countPosts } = useFetch("/api/posts/count", {
   method: "get",
@@ -22,16 +35,10 @@ const {
   refresh: listPosts,
 } = useFetch("/api/posts", {
   method: "get",
+
   immediate: false,
   watch: false,
-  query: {
-    limit: pagination.value.perPage,
-    offset,
-    include: "user",
-    order: "newestFirst",
-    select:
-      "id,title,excerpt,publishedAt,image,user.firstName,user.lastName,user.avatar,user.email",
-  },
+  query: query.value,
 });
 
 const onChangePage = async (page: number) => {
@@ -65,7 +72,7 @@ onMounted(async () => {
         <li>Make it look good 💪</li>
         <li>
           Then
-          <NuxtLink class="text-blue-500 underline" to="/posts/hello">
+          <NuxtLink class="text-indigo-500 underline" to="/posts/hello">
             go to the next task (displaying the individual post)</NuxtLink
           >
         </li>
@@ -73,7 +80,9 @@ onMounted(async () => {
     </div>
   </div>
   <div class="container py-12">
-    <h1 class="text-5xl leading-relaxed font-sans text-center font-extrabold text-gray-800">
+    <h1
+      class="text-5xl leading-relaxed font-sans text-center font-extrabold text-gray-800"
+    >
       Our Blog
     </h1>
     <h4
@@ -82,6 +91,121 @@ onMounted(async () => {
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, deleniti
       vel sunt quos excepturi earum aspernatur perferendis.
     </h4>
+    <div class="flex justify-between items-center mb-4">
+      <div
+        class="bg-gray-200 text-sm text-gray-500 leading-none border-2 border-gray-200 rounded-md inline-flex"
+      >
+        <button
+          id="grid"
+          class="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none border border-transparent hover:text-indigo-400 focus:text-indigo-400 rounded-l-md p-2"
+          :class="[
+            blogView === 'grid' &&
+              'bg-white text-indigo-500 shadow-sm border-indigo-400',
+          ]"
+          @click="blogView = 'grid'"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="fill-current w-4 h-4 mr-2"
+          >
+            <rect x="3" y="3" width="7" height="7"></rect>
+            <rect x="14" y="3" width="7" height="7"></rect>
+            <rect x="14" y="14" width="7" height="7"></rect>
+            <rect x="3" y="14" width="7" height="7"></rect>
+          </svg>
+          <span>Grid</span>
+        </button>
+        <button
+          id="list"
+          class="inline-flex items-center transition-colors duration-300 ease-in focus:outline-none border border-transparent hover:text-indigo-400 focus:text-indigo-400 rounded-r-md p-2"
+          :class="[
+            blogView === 'list' &&
+              'bg-white text-indigo-500 shadow-sm border-indigo-400',
+          ]"
+          @click="blogView = 'list'"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="fill-current w-4 h-4 mr-2"
+          >
+            <line x1="8" y1="6" x2="21" y2="6"></line>
+            <line x1="8" y1="12" x2="21" y2="12"></line>
+            <line x1="8" y1="18" x2="21" y2="18"></line>
+            <line x1="3" y1="6" x2="3.01" y2="6"></line>
+            <line x1="3" y1="12" x2="3.01" y2="12"></line>
+            <line x1="3" y1="18" x2="3.01" y2="18"></line>
+          </svg>
+          <span>List</span>
+        </button>
+      </div>
+      <button
+        class="text-sm text-gray-500 inline-flex items-center gap-1 transition-colors duration-300 ease-in focus:outline-none hover:text-indigo-400 focus:text-indigo-400 p-2"
+        @click="
+          () => {
+            newestFirst = !newestFirst;
+            getPosts();
+          }
+        "
+      >
+        <svg
+          v-if="newestFirst"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-width="1.5"
+          >
+            <path d="M4 8h9" />
+            <path d="M6 13h7" opacity=".7" />
+            <path d="M8 18h5" opacity=".4" />
+            <path stroke-linejoin="round" d="M17 20V4l3 4" />
+          </g>
+        </svg>
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-width="1.5"
+          >
+            <path d="M4 16h9" />
+            <path d="M6 11h7" opacity=".7" />
+            <path d="M8 6h5" opacity=".3" />
+            <path stroke-linejoin="round" d="M17 4v16l3-4" />
+          </g>
+        </svg>
+        <span class="font-bold underline">{{
+          newestFirst ? "Newest" : "Oldest"
+        }}</span>
+      </button>
+    </div>
     <div
       v-if="pending"
       class="text-lg h-screen flex justify-center items-center"
